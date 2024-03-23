16f73a1c15df4dd4f906646fa3bf7ea95d88aa40
anushadevelopment <anushadevelopment@gmail.com>
2024-03-23 19:12:04 +0800

    Customers

---
+++ src/main/java/sg/edu/ntu/javaproject/Implementation/CustomerServiceImpl.java
@@ -0,0 +1,77 @@
+package sg.edu.ntu.javaproject.Implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import sg.edu.ntu.javaproject.Exception.AccountNotFoundException;
+import sg.edu.ntu.javaproject.Exception.CustomerNotFoundException;
+import sg.edu.ntu.javaproject.entity.Account;
+import sg.edu.ntu.javaproject.entity.Customers;
+import sg.edu.ntu.javaproject.repository.CustomerRepository;
+import sg.edu.ntu.javaproject.service.CustomerService;
+import sg.edu.ntu.javaproject.Exception.NullException;
+
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+    
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    
+    public Customers createCustomers(Customers customer){
+        return customerRepository.save(customer);
+    }
+
+
+    @Override
+    public ArrayList<Customers> getAllCustomers() {
+        // TODO Auto-generated method stub
+        List<Customers> allCustomers = customerRepository.findAll();
+        // throw new UnsupportedOperationException("Unimplemented method 'getAllCustomers'");
+        return (ArrayList<Customers>) allCustomers;
+    }
+
+    @Override
+    public Customers getCustomerById(Integer customer_id) {
+        return customerRepository.findById(customer_id).orElseThrow(() -> new CustomerNotFoundException(customer_id));
+       
+}
+    @Override
+    public void deleteCustomerById(Integer customer_id){
+        if (customerRepository.existsById(customer_id)){
+            throw new CustomerNotFoundException(customer_id);
+        }
+         customerRepository.deleteById(customer_id);
+
+    }
+
+    @Override
+    public Customers updateCustomer(Integer customer_id, Customers customer){
+        Customers customerToUpdate = customerRepository.findById(customer_id).orElseThrow(() -> new CustomerNotFoundException(customer_id));
+
+        if (customerToUpdate.getCustomer_name() != null){
+            customerToUpdate.setCustomer_name(customer.getCustomer_name());
+        }
+        if (customerToUpdate.getCustomer_email() != null){
+            customerToUpdate.setCustomer_email(customer.getCustomer_email());
+        }
+        if (customerToUpdate.getCustomer_contact() != null){
+            customerToUpdate.setCustomer_contact(customer.getCustomer_contact());
+        }
+        if (customerToUpdate.getCustomer_address() != null){
+            customerToUpdate.setCustomer_address(customer.getCustomer_address());
+        }
+
+        return customerRepository.save(customerToUpdate);
+
+
+    }
+
+}
---
+++ src/main/java/sg/edu/ntu/javaproject/controller/CustomerController.java
@@ -0,0 +1,75 @@
+package sg.edu.ntu.javaproject.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+import sg.edu.ntu.javaproject.entity.Customers;
+import sg.edu.ntu.javaproject.service.AccountService;
+import sg.edu.ntu.javaproject.service.CustomerService;
+
+@RestController
+@RequestMapping("/customers")
+@Slf4j
+public class CustomerController {
+    private CustomerService customerService;
+    private ObjectMapper objectMapper;
+
+    public CustomerController(CustomerService customerService, ObjectMapper objectMapper){
+        this.customerService = customerService;
+        this.objectMapper = objectMapper;
+    }
+
+@PostMapping("")
+public ResponseEntity<Customers> createCustomer(@RequestBody Customers customer) throws JsonProcessingException{
+    Customers newcustomer = customerService.createCustomers(customer);
+    String customerJson = objectMapper.writeValueAsString(newcustomer);
+    log.info("New customer created : "+customerJson);
+    return new ResponseEntity<>(newcustomer, HttpStatus.CREATED);
+}
+
+
+
+@GetMapping("")
+public ResponseEntity<ArrayList<Customers>> getAllCustomers() throws JsonProcessingException{
+    ArrayList<Customers> allCustomers = customerService.getAllCustomers(); 
+    String customerJson = objectMapper.writeValueAsString(allCustomers);
+    log.info("Retrieved all customers : " +customerJson);
+    return new ResponseEntity<>(allCustomers, HttpStatus.OK);
+}
+@GetMapping({ "/{id}", "/{id}/" })
+public ResponseEntity<Customers> getCustomerById(@PathVariable Integer id) throws JsonProcessingException{
+    Customers customer = customerService.getCustomerById(id);
+    String customerJson = objectMapper.writeValueAsString(customer);
+    log.info("Retrieved Customer By Id : "+ customerJson);
+    return new ResponseEntity<>(customer, HttpStatus.OK);
+}
+@DeleteMapping({"/{id}", "/{id}/"})
+public ResponseEntity<Customers> deleteCustomerById(@PathVariable Integer customer_id) throws JsonProcessingException{
+    customerService.deleteCustomerById(customer_id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+}
+@PutMapping({"/{id}", "/{id}/"})
+public ResponseEntity<Customers> updateCustomer(@PathVariable Integer id, @RequestBody Customers customer) 
+throws JsonProcessingException{
+    Customers customers = customerService.updateCustomer(id, customer);
+
+    return new ResponseEntity<>(HttpStatus.OK);
+
+}
+}
