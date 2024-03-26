814d83b96f35c1d40f3df31cf4b8d54ec7a2b2b9
whlionel704 <whlionel704@gmail.com>
2024-03-26 20:27:05 +0800

    txnsA

--- src/main/java/sg/edu/ntu/javaproject/Exception/TransactionTypeIsNotExistException.java
+++ src/main/java/sg/edu/ntu/javaproject/Exception/TransactionTypeIsNotExistException.java
@@ -1,5 +1,7 @@
 package sg.edu.ntu.javaproject.Exception;
 
-public class TransactionTypeIsNotExistException {
-
+public class TransactionTypeIsNotExistException extends RuntimeException{
+    public TransactionTypeIsNotExistException(Integer id) {
+        super("Transaction Type with ID " + id + " is not exist");
+    }
 }
